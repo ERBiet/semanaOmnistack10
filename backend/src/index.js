c4b15b18c97f6e4ba94687e0ffe408fe212aaf3d
Emanuel <erbiet@outlook.com>
2020-01-14 23:18:17 -0300

    Adicionando mongoose e router

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-j8omi.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+
+app.listen(3333);
