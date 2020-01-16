42e29c1b95ce449dc28aee3b781ad7a44c8a7494
Emanuel <erbiet@outlook.com>
2020-01-15 23:55:05 -0300

    Criando um component de contador com increment

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+//Component: bloco isolado de HTML, CSS e JS que não interfere no restante da aplicação
+//Propriedade: Os famosos atributos do HTML, sã informações que um componente pai passa pra um componente filho
+//Estado: Informações mantidas pelo component (Lembrar: imutabilidade)
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <h1>Contador: {counter}</h1>
+   <button onClick={incrementCounter}>Incrementar</button>
+   </>
   );
 }
 
