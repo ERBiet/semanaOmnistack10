93e9298161408524ad7017d283fbe08ef56024d9
Emanuel <erbiet@outlook.com>
2020-01-17 00:18:58 -0300

    Fazendo a componentização do projeto web, utilizando axios pra pegar as informações da api e passar pra ela ao invés do fetch do js e cors na api

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
-//Component: bloco isolado de HTML, CSS e JS que não interfere no restante da aplicação
-//Propriedade: Os famosos atributos do HTML, sã informações que um componente pai passa pra um componente filho
-//Estado: Informações mantidas pelo component (Lembrar: imutabilidade)
+import './Global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [devs, setDevs] = useState([]);
 
-  function incrementCounter() {
-    setCounter(counter + 1);
+  useEffect(() => {
+    async function loadDevs(){
+        const response = await api.get('/devs');
+
+        setDevs(response.data);
+      };
+
+      loadDevs();
+  }, []);
+
+  async function handleAddDev(data){
+
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
   }
 
   return (
-    <>
-   <h1>Contador: {counter}</h1>
-   <button onClick={incrementCounter}>Incrementar</button>
-   </>
-  );
-}
-
-export default App;
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev}/>
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev =>(
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
+          
+        </ul>
+      </main>
+    </div>
+    );
+  }
+  
+  export default App;
+  
